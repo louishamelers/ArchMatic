58926bc8ed43d03da3f85eab8fc622727e485f21
Louis Hamelers <louis@hamelers.nl>
2020-12-20 15:21:37 +0100

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -8,17 +8,10 @@
 #-------------------------------------------------------------------------
 
 echo "-------------------------------------------------"
-echo "Setting up mirrors for optimal download - US Only"
+echo "Sync clock etc"
 echo "-------------------------------------------------"
 timedatectl set-ntp true
-pacman -S --noconfirm pacman-contrib
-mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
-
-
-
-echo -e "\nInstalling prereqs...\n$HR"
-pacman -S --noconfirm gptfdisk btrfs-progs
+timedatectl status
 
 echo "-------------------------------------------------"
 echo "-------select your disk to format----------------"
