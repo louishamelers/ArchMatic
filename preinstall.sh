ab55bf82aabdd16fc527862c59f420420b2febee
Louis Hamelers <louis@hamelers.nl>
2020-12-20 15:50:16 +0100

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -28,29 +28,34 @@ sgdisk -Z ${DISK} # zap all on disk
 sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
 
 # create partitions
-sgdisk -n 1:0:+1000M ${DISK} # partition 1 (UEFI SYS), default start block, 512MB
-sgdisk -n 2:0:0     ${DISK} # partition 2 (Root), default start, remaining
+sgdisk -n 1:0:+550M ${DISK} # partition 1 (UEFI SYS), default start block, 512MB
+sgdisk -n 2:0:+2G   ${DISK} # partition 2 (SWAP), default start block, 2GB
+sgdisk -n 3:0:0     ${DISK} # partition 3 (Root), default start block, remaining
 
 # set partition types
 sgdisk -t 1:ef00 ${DISK}
-sgdisk -t 2:8300 ${DISK}
+sgdisk -t 2:8200 ${DISK}
+sgdisk -t 3:8300 ${DISK}
 
 # label partitions
 sgdisk -c 1:"UEFISYS" ${DISK}
-sgdisk -c 2:"ROOT" ${DISK}
+sgdisk -c 2:"SWAP" ${DISK}
+sgdisk -c 3:"ROOT" ${DISK}
 
 # make filesystems
 echo -e "\nCreating Filesystems...\n$HR"
 
-mkfs.vfat -F32 -n "UEFISYS" "${DISK}1"
-mkfs.ext4 -L "ROOT" "${DISK}2"
+mkfs.fat -F32 -n "UEFISYS" "${DISK}p1"
+mkswap "${DISK}p2"
+swapon "${DISK}p2"
+mkfs.ext4 -L "ROOT" "${DISK}p3"
 
 # mount target
 mkdir /mnt
-mount -t ext4 "${DISK}2" /mnt
+mount -t ext4 "${DISK}p3" /mnt
 mkdir /mnt/boot
 mkdir /mnt/boot/efi
-mount -t vfat "${DISK}1" /mnt/boot/
+mount -t vfat "${DISK}p1" /mnt/boot/
 
 read -p "Press enter to continue"
 
