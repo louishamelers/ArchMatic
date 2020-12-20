49c9bd115130aaf602e053c02f90f50b12033fe9
Louis Hamelers <louis@hamelers.nl>
2020-12-20 18:52:44 +0100

    Update preinstall-pt2.sh

--- preinstall-pt2.sh
+++ preinstall-pt2.sh
@@ -52,91 +52,12 @@ sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 # install grub
 pacman -S --noconfirm grub
 # configure grub
-mkdir /boot/EFI
-mount /dev/nvme0n1p1 /boot/EFI
+mount /dev/nvme0n1p1 /boot/efi
 grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
 
+pacman -S --noconfirm networkmanager git
+systemctl enable NetworkManager
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # install xorg
-# pacman -S --noconfirm mesa
-# pacman -S --noconfirm xorg
-# pacman -S --noconfirm xorg-xinit
-# # configure xinit
-# echo "exec awesome" > /home/arch/.xinitrc
-
-# # install window manager
-# pacman -S --noconfirm awesome
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # echo "done"
-# # read -p "Press enter to continue"
-
-# echo "--------------------------------------"
-# echo "-- Bootloader Systemd Installation  --"
-# echo "--------------------------------------"
-# bootctl install
-# cat <<EOF > /boot/loader/entries/arch.conf
-# title Arch Linux  
-# linux /vmlinuz-linux  
-# initrd  /initramfs-linux.img  
-# options root=${DISK}1 rw
-# EOF
-
-
-# echo "done"
-# read -p "Press enter to continue"
-
-# echo "--------------------------------------"
-# echo "--          Network Setup           --"
-# echo "--------------------------------------"
-# pacman -S networkmanager dhclient --noconfirm --needed
-# systemctl enable --now NetworkManager
-
-# exit
-# umount -R /mnt
+# done
+exit
