043fd9324373e1d1f69dc1616a67a0eb8c058630
Louis Hamelers <louis@hamelers.nl>
2020-12-24 16:12:33 +0100

    Update 9-post-setup.sh

--- 9-post-setup.sh
+++ 9-post-setup.sh
@@ -51,12 +51,12 @@ sudo sed -i 's|xserverauthfile=\$HOME/.serverauth.\$\$|xserverauthfile=\$XAUTHOR
 
 # ------------------------------------------------------------------------
 
-# echo -e "\nConfiguring LTS Kernel as a secondary boot option"
+echo -e "\nConfiguring LTS Kernel as a secondary boot option"
 
-# sudo cp /boot/loader/entries/arch.conf /boot/loader/entries/arch-lts.conf
-# sudo sed -i 's|Arch Linux|Arch Linux LTS Kernel|g' /boot/loader/entries/arch-lts.conf
-# sudo sed -i 's|vmlinuz-linux|vmlinuz-linux-lts|g' /boot/loader/entries/arch-lts.conf
-# sudo sed -i 's|initramfs-linux.img|initramfs-linux-lts.img|g' /boot/loader/entries/arch-lts.conf
+sudo cp /boot/loader/entries/arch.conf /boot/loader/entries/arch-lts.conf
+sudo sed -i 's|Arch Linux|Arch Linux LTS Kernel|g' /boot/loader/entries/arch-lts.conf
+sudo sed -i 's|vmlinuz-linux|vmlinuz-linux-lts|g' /boot/loader/entries/arch-lts.conf
+sudo sed -i 's|initramfs-linux.img|initramfs-linux-lts.img|g' /boot/loader/entries/arch-lts.conf
 
 # ------------------------------------------------------------------------
 
